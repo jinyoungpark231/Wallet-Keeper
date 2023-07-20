3aeb501858c14d530024be434e952fe7ab9552ee
ChaDongmin <cdm1263@gmail.com>
2023-07-20 22:05:36 +0900

    FIX : port 및 proxy 삭제

--- vite.config.ts
+++ vite.config.ts
@@ -3,15 +3,5 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  server: {
-    proxy: {
-      '/api': {
-        target: 'https://chickenlecture.xyz/',
-        changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api/, '')
-      }
-    },
-    port: 3000
-  }
+  plugins: [react()]
 });
